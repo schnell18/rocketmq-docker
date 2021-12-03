4eb6faae73c00b70115bdb67b29efc597bf515c5
Justin Zhang <schnell18@gmail.com>
2021-12-03 11:39:39 +0800

    Bump up rev to 2

--- image-build/combine.sh
+++ image-build/combine.sh
@@ -1,6 +1,7 @@
 ROCKETMQ_VERSION=4.9.2
-docker manifest create schnell18/rocketmq:${ROCKETMQ_VERSION}-alpine \
-    --amend schnell18/rocketmq:${ROCKETMQ_VERSION}-alpine-arm64 \
-    --amend schnell18/rocketmq:${ROCKETMQ_VERSION}-alpine-amd64
+REV=2
+docker manifest create schnell18/rocketmq:${ROCKETMQ_VERSION}-${REV}-alpine \
+    --amend schnell18/rocketmq:${ROCKETMQ_VERSION}-${REV}-alpine-arm64 \
+    --amend schnell18/rocketmq:${ROCKETMQ_VERSION}-${REV}-alpine-amd64
 
-docker manifest push schnell18/rocketmq:${ROCKETMQ_VERSION}-alpine
+docker manifest push schnell18/rocketmq:${ROCKETMQ_VERSION}-${REV}-alpine
